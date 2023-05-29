15d4c9b4737fba0fc6a9b3dcee457041cb385d29
Pedrosmp <pedrosmp1@al.insper.edu.br>
2023-05-29 10:15:29 -0300

    funcoes

--- jogo.py
+++ jogo.py
@@ -22,3 +22,23 @@ font = pygame.font.SysFont(None,45)
 font_1 = pygame.font.SysFont(None,35)
 text_font=pygame.font.Font(None,80)
 times = 0
+def font_screen(text,color, x, y):
+    pong_text = font.render(text, True, color)
+    screen.blit(pong_text, [x,y])
+
+def font_screen_1(text,color,x,y):
+    enter_text = font_1.render(text,True,color)
+    screen.blit(enter_text, [x,y])
+
+def welcome_screen():
+    exit_game= False
+    while not exit_game:
+        screen.blit(start_image, [0,0])
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit_game = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    loop()
+        pygame.display.update()
+        clock.tick(60)
